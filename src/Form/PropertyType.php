7f61dea1c96ac898bf676127a5253868bc7d978d
Steven Lignereux <steven.lignereux@hotmail.fr>
2019-03-08 16:06:22 +0100

    feat:thub system

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -6,6 +6,7 @@ use App\Entity\Option;
 use App\Entity\Property;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -27,8 +28,12 @@ class PropertyType extends AbstractType
             ])
             ->add('options', EntityType::class, [
                 'class' => Option::class,
+                'required' => false,
                 'choice_label' => 'name',
                 'multiple' => true
+            ])
+            ->add('imageFile', FileType::class, [
+                'required' => false
             ])
             ->add('city')
             ->add('address')
